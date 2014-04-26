dbb5af6cad19c518645910e012b6dfb3237b27ab
Ilya Dyakonov <iluskadyakonov@gmail.com>
2014-04-26 20:25:03 +0200

    fixed naming

---
+++ Categories/UIResponder+ZLCFirstResponder.m
@@ -0,0 +1,33 @@
+//
+// Created by Ilya Dyakonov on 17/12/13.
+// Copyright (c) 2013 ZappyLab. All rights reserved.
+//
+//
+
+
+#import "UIResponder+ZLCFirstResponder.h"
+
+/////////////////////////////////////////////////////
+
+static __weak id ZLC_currentFirstResponder;
+
+@implementation UIResponder (ZLCFirstResponder)
+
++(id) ZLC_currentFirstResponder
+{
+    ZLC_currentFirstResponder = nil;
+    [[UIApplication sharedApplication] sendAction:@selector(ZLC_findFirstResponder:)
+                                               to:nil
+                                             from:nil
+                                         forEvent:nil];
+    return ZLC_currentFirstResponder;
+}
+
+-(void) ZLC_findFirstResponder:(id) sender
+{
+    ZLC_currentFirstResponder = self;
+}
+
+@end
+
+/////////////////////////////////////////////////////
