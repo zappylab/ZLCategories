25ed73d7d3e570d9cbdd257ae87dfbcec9a0f970
Ilya Dyakonov <iluskadyakonov@gmail.com>
2014-05-11 21:04:12 +0200

    category clearing NSDictionaries from NSNulls

---
+++ Categories/NSDictionary+ClearFromNulls.m
@@ -0,0 +1,29 @@
+//
+// Created by Ilya Dyakonov on 11/02/14.
+// Copyright (c) 2014 ZappyLab. All rights reserved.
+//
+//
+
+
+#import "NSDictionary+ZLClearFromNulls.h"
+
+/////////////////////////////////////////////////////
+
+@implementation NSDictionary (ZLClearFromNulls)
+
+-(NSDictionary *) ZL_clearFromNulls
+{
+    NSMutableDictionary *mutableDictionary = [self mutableCopy];
+    for (id key in mutableDictionary.allKeys) {
+        id object = [mutableDictionary objectForKey:key];
+        if ([object isKindOfClass:[NSNull class]]) {
+            [mutableDictionary removeObjectForKey:key];
+        }
+    }
+
+    return [NSDictionary dictionaryWithDictionary:mutableDictionary];
+}
+
+@end
+
+/////////////////////////////////////////////////////
---
+++ Categories/NSDictionary+ZLClearFromNulls.h
@@ -0,0 +1,14 @@
+//
+// Created by Ilya Dyakonov on 11/02/14.
+// Copyright (c) 2014 ZappyLab. All rights reserved.
+//
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (ZLClearFromNulls)
+
+-(NSDictionary *) ZL_clearFromNulls;
+
+@end
