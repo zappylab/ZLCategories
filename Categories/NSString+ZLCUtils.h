dd9587de85255fcadac905cf5a598390b6ae3519
Ilya Dyakonov <iluskadyakonov@gmail.com>
2015-03-24 11:04:08 +0300

    string utils

---
+++ Categories/NSString+ZLCUtils.h
@@ -0,0 +1,21 @@
+//
+// Created by Ilya Dyakonov on 11/09/14.
+// Copyright (c) 2014 ZappyLab. All rights reserved.
+//
+//
+
+
+#import <Foundation/Foundation.h>
+
+/////////////////////////////////////////////////////
+
+@interface NSString (ZLCUtils)
+
+-(BOOL) zlc_ableToReplaceTextInRange:(NSRange) range;
+-(NSString *) zlc_stringByRemovingChainedNewlines;
+-(NSString *) zlc_stringByRemovingHTMLTags;
+-(NSString *) zlc_stringByRemovingEncodedCharacters;
+
+@end
+
+/////////////////////////////////////////////////////
---
+++ Categories/NSString+ZLCUtils.m
@@ -0,0 +1,82 @@
+//
+// Created by Ilya Dyakonov on 11/09/14.
+// Copyright (c) 2014 ZappyLab. All rights reserved.
+//
+//
+
+
+#import "NSString+ZLCUtils.h"
+
+/////////////////////////////////////////////////////
+
+@implementation NSString (ZLCUtils)
+
+-(BOOL) zlc_ableToReplaceTextInRange:(NSRange) range
+{
+    return range.location + range.length <= self.length;
+}
+
+#pragma mark - Chained newlines
+
+-(NSString *) zlc_stringByRemovingChainedNewlines
+{
+    NSError *error = nil;
+    NSRegularExpression *chainedNewlinesExpression = [NSRegularExpression regularExpressionWithPattern:@"\n\n+"
+                                                                                               options:NSRegularExpressionCaseInsensitive
+                                                                                                 error:&error];
+    NSString *stringWithCollapsedNewlineSequences = [chainedNewlinesExpression stringByReplacingMatchesInString:self
+                                                                                                        options:kNilOptions
+                                                                                                          range:NSMakeRange(0, self.length)
+                                                                                                   withTemplate:@"\n"];
+    return [stringWithCollapsedNewlineSequences zlc_stringByRemovingPrefixingNonAlphanumericCharacters];
+}
+
+-(NSString *) zlc_stringByRemovingPrefixingNonAlphanumericCharacters
+{
+    NSString *result = nil;
+    NSRange firstAlphanumericCharacterGroupRange = [self rangeOfCharacterFromSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if (firstAlphanumericCharacterGroupRange.location == 0)
+    {
+        // string has non-alphanumeric prefix
+        result = [self stringByReplacingCharactersInRange:firstAlphanumericCharacterGroupRange
+                                               withString:@""];
+    }
+    else
+    {
+        result = self;
+    }
+
+    return result;
+}
+
+#pragma mark - HTML tags
+
+-(NSString *) zlc_stringByRemovingHTMLTags
+{
+    NSError *error = nil;
+    NSRegularExpression *htmlTagExpression = [NSRegularExpression regularExpressionWithPattern:@"<[^>]+>"
+                                                                                       options:NSRegularExpressionCaseInsensitive
+                                                                                         error:&error];
+    NSString *stringWithoutTags = [htmlTagExpression stringByReplacingMatchesInString:self
+                                                                              options:kNilOptions
+                                                                                range:NSMakeRange(0, self.length)
+                                                                         withTemplate:@""];
+    return [stringWithoutTags zlc_stringByRemovingEncodedCharacters];
+}
+
+-(NSString *) zlc_stringByRemovingEncodedCharacters
+{
+    NSError *error = nil;
+    NSRegularExpression *encodedSymbolsExpression = [NSRegularExpression regularExpressionWithPattern:@"&[^;]+;"
+                                                                                              options:NSRegularExpressionCaseInsensitive
+                                                                                                error:&error];
+    return [encodedSymbolsExpression stringByReplacingMatchesInString:self
+                                                              options:kNilOptions
+                                                                range:NSMakeRange(0, self.length)
+                                                         withTemplate:@" "];
+
+}
+
+@end
+
+/////////////////////////////////////////////////////
