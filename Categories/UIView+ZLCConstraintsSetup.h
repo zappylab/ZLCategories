dcc3cd041ce2c5705852e588720760efe86f64b0
Yulia Kurnosova <Yulia-Kurnosova@yandex.ru>
2017-03-11 03:39:06 +0300

    added ability to setup indents for vertical alignment

--- Categories/UIView+ZLCConstraintsSetup.h
+++ Categories/UIView+ZLCConstraintsSetup.h
@@ -2,8 +2,6 @@
 // Created by Ilya Dyakonov on 27/05/14.
 // Copyright (c) 2014 ZappyLab. All rights reserved.
 //
-//
-
 
 #import <Foundation/Foundation.h>
 
@@ -14,7 +12,11 @@
 -(void) ZLC_bindSubviewVertically:(UIView *) subview;
 -(void) ZLC_bindSubviewHorizontally:(UIView *) subview;
 
--(void) ZLC_centerSubview:(UIView *) subview;
+-(void) zlc_bindSubviewVertically:(UIView *) superView
+                  upperEdgeIndent:(NSInteger) upperEdgeIndent
+                 bottomEdgeIndent:(NSInteger) bottomEdgeIndent;
+
+-(void) zlc_centerSubview:(UIView *) subview;
 
 -(NSLayoutConstraint *) ZLC_bindWidth:(CGFloat) width;
 -(NSLayoutConstraint *) ZLC_constraintForEqualWidthsWithView:(UIView *) view;
